17b1005e27e9939e67afaa0a781edf3bd6a1d2c7
chatgris <chatgri@gmail.com>
2010-11-06 22:33:47 +0100

    Typo of ze day

--- lib/jammit/lite/helper.rb
+++ lib/jammit/lite/helper.rb
@@ -11,7 +11,7 @@ module Jammit
       # in production, creates <script> tag for each bundle
       def include_javascripts(*bundles)
         bundles.map! { |name| Jammit::Lite::Bundle.new(:javascripts => name)  }
-        #return include_individual_javascripts(bundles) unless Rails.env.production?
+        return include_individual_javascripts(bundles) unless Rails.env.production?
         tags = bundles.map { |bundle| javascript_include_tag(bundle.path) }
         html_safe(tags.join("\n"))
       end
